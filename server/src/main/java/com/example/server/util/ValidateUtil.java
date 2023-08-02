6c23c8fd8e8bab1ab8511eea794d2634ff8736eb
LinkyHoungla <baisss2000@sina.com>
2023-08-02 14:35:57 +0800

    [增添] 参数校验工具类

--- server/src/main/java/com/example/server/util/ValidateUtil.java
+++ server/src/main/java/com/example/server/util/ValidateUtil.java
@@ -1,9 +1,11 @@
 package com.example.server.util;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.regex.Pattern;
 
-public class IpUtil {
+public class ValidateUtil {
 
+    // FUNCTION IP地址获取
     public static String getClientIpAddress(HttpServletRequest request) {
         // 优先获取 X-Forwarded-For 请求头
         String xForwardedForHeader = request.getHeader("X-Forwarded-For");
@@ -22,4 +24,27 @@ public class IpUtil {
         // 若没有 X-Forwarded-For 和 X-Real-IP 请求头，则直接从请求中获取客户端 IP 地址
         return request.getRemoteAddr();
     }
+
+    // FUNCTION 长度校验
+    public static Boolean length(int min, int max, String str) {
+        return str.length() > min && str.length() < max;
+    }
+    // FUNCTION 范围校验
+    public static Boolean rangeInt(int min, int max, int num) {
+        return num > min && num < max;
+    }
+    public static Boolean rangeFloat(float min, float max, float num) {
+        return num > min && num < max;
+    }
+    // FUNCTION 手机号校验
+    public static Boolean phone(String phone) {
+        String pattern = "^(13[0-9]|14[01456879]|15[0-35-9]|16[2567]|17[0-8]|18[0-9]|19[0-35-9])\\d{8}$";
+        return Pattern.matches(pattern, phone);
+    }
+    // FUNCTION 邮箱校验
+    public static Boolean email(String email) {
+        String pattern = "^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$";
+        return Pattern.matches(pattern, email);
+    }
+
 }
